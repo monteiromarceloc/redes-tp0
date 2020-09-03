1ed02273558fdcb112764955910dd40fdc238008
Marcelo Monteiro <monteiromarceloc@gmail.com>
2020-09-02 23:44:32 -0300

    client and server compiled

--- tutorial/client.c
+++ tutorial/client.c
@@ -1,34 +1,37 @@
+#include "common.h"
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 
 #define BUFSZ 1024
 
-void usage() {
-    printf("usage: %s <server IP> <server port>");
-    printf("example: %s: 127.0.0.1 51511");
+void usage(int argc, char **argv) {
+    printf("usage: %s <server IP> <server port>\n", argv[0]);
+    printf("example: %s: 127.0.0.1 51511\n", argv[0]);
     exit(EXIT_FAILURE);
 }
 
-void logexit(const char *str) {
-	perror(str);
-	exit(EXIT_FAILURE);
-}
+int main(int argc, char **argv) {
+    if (argc < 3) usage(argc, argv);
 
-void main(int argc, char **argv) {
-    if (argc < 3) usage();
+    struct sockaddr_storage storage; // Handle socket ipv4 or ipv6
+    if(0 != addrparse(argv[1], argv[2], &storage)) usage(argc, argv);
 
-    // Conexão ativa
-    int s = socket(AF_INET, SOCK_STREAM, 0);
+    int s = socket(storage.ss_family, SOCK_STREAM, 0);
     if(s == -1) logexit("socket");
-    if(0 != connect(s, addr, sizeof(addr))) logexit("connect");
+
+    struct sockaddr *addr = (struct sockaddr *)(&storage);
+    if(0 != connect(s, addr, sizeof(storage))) logexit("connect"); // storage has size of addr content
 
     char addrstr[BUFSZ];
     addrtostr(addr, addrstr, BUFSZ);
-    print("connected to %s\n");
+    printf("connected to %s\n", addrstr);
 
     char buf[BUFSZ];
     memset(buf, 0, BUFSZ); // Inicializar buffer com 0
@@ -37,6 +40,7 @@ void main(int argc, char **argv) {
     int count = send(s, buf, strlen(buf)+1, 0); // (socket, dado, tamanho, op)
     if(count != strlen(buf)+1) logexit("send"); // count = numero de bytes efetivamente transmitido
 
+    // recv
     memset(buf, 0, BUFSZ);
     unsigned total = 0;
     while(1){
@@ -47,5 +51,5 @@ void main(int argc, char **argv) {
     close(s);
     printf("recieved %u bytes\n", total);
     exit(EXIT_SUCCESS);
-
+    return 0;
 }
---
+++ tutorial/common.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stdlib.h>
+#include <arpa/inet.h>
+
+void logexit(const char *str);
+
+int addrparse(const char*addrstr, const char *portstr, struct sockaddr_storage *storage);
+
+void addrtostr(const struct sockaddr *addr, char *str, size_t strsize);
+
+int server_sockaddr_init(const char *proto, const char *portstr, struct sockaddr_storage *storage);
